441b507b3f91c478e352065000ebdbb468b16cca
sukeko <venusgeneration@gmail.com>
2020-08-23 09:16:41 +0900

    kannsei??

--- build/test/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-it("sukeko", function () {
-    expect(1).toBe(1);
-});
--- js/index.ts
+++ js/index.ts
@@ -1,11 +1,12 @@
 const tileSize = 20;
 
-let mX = 5;
-let mY = 2;
+let mX = new Array();
+let mY = new Array();
 let mDX = 0;
 let mDY = 1;
 let mFoodX = 2;
 let mFoodY = 5;
+let gameOver: boolean;
 
 let timerInterval = 100;
 
@@ -17,9 +18,21 @@ function addFood() {
 
   mFoodX = random(tileSize, w - tileSize * 2, tileSize);
   mFoodY = random(tileSize, h - tileSize * 2, tileSize);
+
+  mX.push(mX[mX.length - 1]);
+  mY.push(mY[mY.length - 1]);
 }
 
-export function random(min: number, max: number, mult: number): number {
+function isBody(x, y) {
+  for (let i = mX.length - 1; i >= 0; i--) {
+    if (mX[i] == x && mY[i] == y) {
+      return true;
+    }
+    return false;
+  }
+}
+
+/*export*/ function random(min: number, max: number, mult: number): number {
   let w = max - min;
   let num = min + Math.floor(Math.random() * w);
   return num - (num % mult);
@@ -39,14 +52,39 @@ function draw(cvs: HTMLCanvasElement) {
   ctx.fillStyle = "red";
   ctx.fillRect(mFoodX, mFoodY, tileSize, tileSize);
 
-  ctx.fillStyle = "yellow";
-  ctx.fillRect(mX * tileSize, mY * tileSize, tileSize, tileSize);
+  for (let i = mX.length - 1; i >= 0; i--) {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(mX[i] * tileSize, mY[i] * tileSize, tileSize, tileSize);
+  }
+
+  ctx.fillStyle = "#fff";
+  ctx.font = "24px MS Gothic";
+  if (gameOver) {
+    ctx.fillText("GAME OVER", w / 2 - 100, h / 2 - 50);
+  }
 }
 
 function update() {
-  mX += mDX;
-  mY += mDY;
-  if (mX * tileSize === mFoodX && mY * tileSize === mFoodY) {
+  if (gameOver) {
+    return;
+  }
+  let x = (mX[0] += mDX);
+  let y = (mY[0] += mDY);
+
+  if (
+    isBody(x, y) ||
+    x < 1 ||
+    y < 1 ||
+    x * tileSize > 1420 - 40 ||
+    y * tileSize > 760 - 40
+  ) {
+    gameOver = true;
+  }
+  mX.unshift(x);
+  mY.unshift(y);
+  mX.pop();
+  mY.pop();
+  if (x * tileSize === mFoodX && y * tileSize === mFoodY) {
     addFood();
   }
 }
@@ -77,6 +115,8 @@ window.onkeydown = function (ev: { keyCode: any }) {
 };
 
 window.onload = function () {
+  mX.push(5);
+  mY.push(2);
   addFood();
   setInterval(onPaint, timerInterval);
 };
